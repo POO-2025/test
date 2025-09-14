e63501a985aa025164b5d59271e495c6521064e6
Alex <alex@>
2025-09-13 22:27:27 -0300

    tercer commit

--- main.java
+++ main.java
@@ -2,5 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hola como estan");
         System.out.println("Soy alexandra");
+        System.out.println("Estamos con sadrach");
+        System.out.println("bastian y hans");
     }
 }
